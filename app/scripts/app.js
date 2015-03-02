0bb76d6811135e274bbe7f5a66e2e152ab6a0978
Y.Y. Huang <foogwai@gmail.com>
2015-03-02 15:57:18 +0800

    add ng-file-upload library

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angularFileUpload'
   ])
     .run(function($rootScope, $location) {
         $rootScope.getClass = function (path) {
--- app/scripts/controllers/file.js
+++ app/scripts/controllers/file.js
@@ -7,7 +7,44 @@
  * # FileCtrl
  * Controller of the nrichApp
  */
-angular.module('nrichApp')
-  .controller('FileCtrl', function ($scope) {
+var app = angular.module('nrichApp');
+app.controller('FileCtrl', function ($scope, $location, $upload) {
+        $scope.isActive = function(tab) {
+            var qry_type = $location.search().type;
+            if ((qry_type == undefined) || (qry_type == "")){
+                qry_type = "upload";
+            }
+            if (qry_type == tab)
+                return "active";
+            else
+                return "";
+        };
 
+        $scope.$watch('files', function(){
+            $scope.upload($scope.files);
+        });
+
+        $scope.upload = function(files) {
+            if (files && files.length){
+                for(var i = 0; i < files.length; i++){
+                    var file = files[i];
+                    $upload.upload({
+                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                        fields: {
+                            'username': $scope.username
+                        },
+                        file: file
+                    }).progress(function (evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        console.log('progress: ' + progressPercentage + '% ' +
+                            evt.config.file.name);
+                    }).success(function (data, status, headers, config) {
+                        console.log('file ' + config.file.name + 'uploaded. Response: ' +
+                            JSON.stringify(data));
+                    });
+                }
+            }
+        }
   });
+
+
