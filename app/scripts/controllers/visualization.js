42b1934f69985e12f8aa26a445179e4516f3b383
Y.Y. Huang <foogwai@gmail.com>
2015-03-06 18:39:27 +0800

    modified:   app/scripts/controllers/visualization.js 	modified:   app/styles/main.css 	modified:   app/views/file.html 	modified:   app/views/visualization.html

--- app/scripts/controllers/visualization.js
+++ app/scripts/controllers/visualization.js
@@ -21,44 +21,101 @@ angular.module('nrichApp')
         };
         $scope.options = {
             chart: {
-                type: 'discreteBarChart',
-                height: 450,
-                width: 840,
-                margin : {
-                    top: 20,
+                type: 'multiBarChart',
+                height: 250,
+                width: 900,
+                margin: {
+                    top: 200,
                     right: 20,
-                    bottom: 60,
-                    left: 55
+                    bottom: 20,
+                    left: 90
                 },
-                x: function(d){ return d.label; },
-                y: function(d){ return d.value; },
+                x: function(d){return d.label;},
+                y: function(d){return d.value;},
+                showControls: false,
                 showValues: true,
-                valueFormat: function(d){
-                    return d3.format(',.4f')(d);
-                },
                 transitionDuration: 500,
                 xAxis: {
-                    axisLabel: 'X Axis'
+                    showMaxMin: false
                 },
                 yAxis: {
-                    axisLabel: 'Record Count',
-                    axisLabelDistance: 30
+                    axisLabel: 'Record Counts',
+                    tickFormat: function(d){
+                        return d3.format(',d')(d);
+                    }
                 }
             }
         };
 
-        $scope.data = [{
-            key: "Cumulative Return",
-            values: [
-                { "label" : "A" , "value" : -29.765957771107 },
-                { "label" : "B" , "value" : 0 },
-                { "label" : "C" , "value" : 32.807804682612 },
-                { "label" : "D" , "value" : 196.45946739256 },
-                { "label" : "E" , "value" : 0.19434030906893 },
-                { "label" : "F" , "value" : -98.079782601442 },
-                { "label" : "G" , "value" : -13.925743130903 },
-                { "label" : "H" , "value" : -5.1387322875705 }
-            ]
-        }];
+        $scope.data = [
+            {
+                "key": "Your File",
+                "color": "#3c97ac",
+                "values": [
+                    {
+                        "label" : "Email" ,
+                        "value" : 1200345
+                    } ,
+                    {
+                        "label" : "First" ,
+                        "value" : 94756
+                    } ,
+                    {
+                        "label" : "Last" ,
+                        "value" : 93342
+                    } ,
+                    {
+                        "label" : "State" ,
+                        "value" : 53120
+                    }
+                ]
+            },
+            {
+                "key": "NRich",
+                "color": "#89a24c",
+                "values": [
+                    {
+                        "label" : "Email" ,
+                        "value" : 399753
+                    } ,
+                    {
+                        "label" : "First" ,
+                        "value" : 350000
+                    } ,
+                    {
+                        "label" : "Last" ,
+                        "value" : 345734
+                    } ,
+                    {
+                        "label" : "State" ,
+                        "value" : 219546
+                    }
+                ]
+            }
 
+        ];
+        var file1 = new Object();
+        file1.name = "AncientRuins.csv";
+        file1.size = "1.2MB, 245,653 Records";
+        file1.modified = "Updated on 6/10/2014 12:34";
+        file1.cleansed = "Cleansed on 6/13/2014 9:21";
+        file1.records = "399,753 records";
+        file1.percent = "33%";
+        var file2 = new Object();
+        file2.name = "JakesWillow.csv";
+        file2.size = "256KB, 53,034 Records";
+        file2.modified = "Updated on 8/20/2014 9:14";
+        file2.cleansed = "";
+        file2.percent = "28%";
+        file2.records = "71,693 records";
+
+        var files = new Array();
+        files.push(file1);
+        files.push(file2);
+        $scope.files = files;
+
+        $scope.toggle = function() {
+            $scope.hideVisual = !$scope.hideVisual;
+        };
+        $scope.hideVisual = true;
 });
